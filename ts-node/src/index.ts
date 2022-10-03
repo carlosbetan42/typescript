367aef5a9844f4e7a7ccda5327507c931e6830e1
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2022-10-03 15:28:21 -0500

    Namespaces

---
+++ ts-node/src/index.ts
@@ -0,0 +1,10 @@
+// import * as HeroClasses from './classes/hero';
+// import powers, { Power } from './data/powers';
+// const Hero = 123;
+// const ironman = new HeroClasses.Hero('Ironman', 1, 55);
+// console.log(ironman);
+// console.log(powers[0]);
+
+import { Hero, Hero2 } from './classes/hero';
+const ironman = new Hero('Ironman', 10, 55);
+console.log(ironman.power);
