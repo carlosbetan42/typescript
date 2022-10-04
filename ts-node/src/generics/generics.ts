47dfa74e5c04d21606be6a0b867c655ce9d59fae
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2022-10-04 10:33:57 -0500

    Generics

---
+++ ts-node/src/generics/generics.ts
@@ -0,0 +1,9 @@
+export const printObject = (argument: any) => {
+  console.log(argument);
+};
+
+export function genericFuntion<T>(argument: T): T {
+  return argument;
+}
+
+export const genericFunctionArrow = <T>(argument: T): T => argument;
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,10 +1,6 @@
-// import * as HeroClasses from './classes/hero';
-// import powers, { Power } from './data/powers';
-// const Hero = 123;
-// const ironman = new HeroClasses.Hero('Ironman', 1, 55);
-// console.log(ironman);
-// console.log(powers[0]);
+import { getPokemon } from './generics/get-pokemon';
 
-import { Hero, Hero2 } from './classes/hero';
-const ironman = new Hero('Ironman', 10, 55);
-console.log(ironman.power);
+getPokemon(4)
+  .then((pokemon) => console.log(pokemon.sprites.front_default))
+  .catch((error) => console.error(error))
+  .finally(() => console.log('Fin de getPokemon'));
